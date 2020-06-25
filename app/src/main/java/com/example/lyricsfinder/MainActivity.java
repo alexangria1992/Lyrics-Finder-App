cdc1bb920181d7b6f0ba3caeb76462bee1cbb56c
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-25 21:47:57 +1200

    Created event handler, request queue, jsonObjectrequest method

--- app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.lyricsfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtArtistName, edtSongName;
     Button btnGetLyrics;
     TextView txtLyrics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
         edtSongName = findViewById(R.id.edtSongName);
         btnGetLyrics = findViewById(R.id.btnGetLyrics);
         txtLyrics = findViewById(R.id.txtLyrics);
+
+        btnGetLyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Toast.makeText(getApplicationContext(), "This button is tapped", Toast.LENGTH_SHORT).show();
+                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" + edtSongName.getText().toString();
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        txtLyrics.setText(response.toString());
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
     }
 }
