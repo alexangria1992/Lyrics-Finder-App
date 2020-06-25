af4094a5ee41d7c03ec9c617a9d183726d4f767a
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-25 21:10:52 +1200

    Created Design, initial variables and depencies (volley)

--- app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lyricsfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtArtistName, edtSongName;
+    Button btnGetLyrics;
+    TextView txtLyrics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtArtistName = findViewById(R.id.edtArtistName);
+        edtSongName = findViewById(R.id.edtSongName);
+        btnGetLyrics = findViewById(R.id.btnGetLyrics);
+        txtLyrics = findViewById(R.id.txtLyrics);
     }
 }
